57ebecfff5180a21a9056390522926eed327b5ec
k1nz <772512760@qq.com>
2022-01-09 23:39:13 +0800

    fix: add axios interface

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,6 +1,13 @@
+import { AxiosRequestConfig } from 'axios';
 declare module '*.vue' {
   import { DefineComponent } from 'vue';
   // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
   const component: DefineComponent<{}, {}, any>;
   export default component;
+}
+
+declare module 'axios' {
+  interface AxiosInstance {
+    (config: AxiosRequestConfig): Promise<any>;
+  }
 }
