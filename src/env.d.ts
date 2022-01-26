af9a9e28c211c821e809462497e8c49d68dbeb76
K1nZ <k1nz.qiu@foxmail.com>
2022-01-26 15:14:52 +0800

    fix(env): importMeta duplicate declare

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,10 +1,11 @@
 /// <reference types="vite/client" />
 
-// interface ImportMetaEnv extends Readonly<Record<string, string>> {
-//   readonly VITE_APP_VERSION: string;
-//   readonly VITE_APP_BUILD_EPOCH?: string;
-// }
+interface ImportMetaEnv {
+  readonly VITE_APP_TITLE: string;
+  readonly VITE_APP_VERSION: string;
+  readonly VITE_APP_BUILD_EPOCH?: string;
+}
 
-// interface ImportMeta {
-//   readonly env: ImportMetaEnv;
-// }
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
